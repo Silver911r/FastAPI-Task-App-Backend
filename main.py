4855d6bb8192c44b7df6e837e59914fe61b4916d
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-14 18:28:09 -0500

    create read route

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from model import Task
+from schema import task_schema
+from session import create_get_session, get_database_session
 
 app = FastAPI()
 
@@ -6,3 +11,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Server is up and runnning!"}
+
+
+@app.get("/task", response_model=List[task_schema], status_code=200)
+async def read_tasks(db: Session = Depends(create_get_session)):
+    tasks = db.query(Task).all()
+    return tasks
