04aaf6ca08a2f36606ba107e185c98fa80fec84e
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-14 18:19:24 -0500

    fixed model.py, schema instead of model

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
-from typing import Optional
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String, Integer, Text
+from database import Base
 
 
-class task_schema(BaseModel):
-    task_name: str
-    task_des: str
-    created_by: Optional[str] = None
-    date_created: Optional[str] = None
-
-    class Config:
-        orm_mode = True
+class Task(Base):
+    __tablename__ = "Tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    task_name = Column(String(20))
+    task_des = Column(Text())
+    created_by = Column(String(20))
+    date_created = Column(String(15))
