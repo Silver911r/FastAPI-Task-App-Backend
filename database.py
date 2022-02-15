9c9ab9b0ce1b1b35d6332973b1ceaa585fb0390b
Silver911r <30500702+Silver911r@users.noreply.github.com>
2022-02-14 19:59:37 -0500

    rename database

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///census.sqlite"
+SQLALCHEMY_DATABASE_URL = "sqlite:///tasks.sqlite"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
